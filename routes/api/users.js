9ad6bc759b9758e9149e63af51159fdabcd042f9
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-06-24 12:07:35 -0700

    Created user login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -38,4 +38,28 @@ router.post('/register', (req, res) => {
     })
 })
 
+// @route GET api/users/login
+// @desc Login user / return jwt token
+// @access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find user by email
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ email: 'User not found!' })
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' })
+          } else {
+            return res.status(400).json({ password: 'Password incorrect' })
+          }
+        })
+    })
+})
 module.exports = router;
