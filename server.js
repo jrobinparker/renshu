c8f566b1e1e5eae19acc31deb586996111dcb09a
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-06-24 11:30:53 -0700

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Setup
+const db = require('./config/keys').mongoURI;
+
+// Connect to DB
+mongoose.connect(db).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('hello world'))
 
