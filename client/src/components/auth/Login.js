9db8cdda14864ae7d32603508580411df353f4cd
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-06-25 09:54:17 -0700

    Created login form.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,47 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    name: '',
+    email: '',
+    errors: {}
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
+
     return (
-      <div>
-      login
+      <div className="login with-shadow">
+        <h1>Login to Your Account</h1>
+        <form noValidate className="ui form">
+        <div className="field">
+            <label>Email</label>
+                <input
+                  label="Email"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  onChange={this.handleChange}
+                  value={this.state.email}
+                />
+          </div>
+          <div className="field">
+            <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.handleChange}
+                value={this.state.password}
+              />
+          </div>
+          <button className="ui violet button" type="submit" style={{ width: '100%', marginTop: '10px' }}>Login</button>
+        </form>
       </div>
     )
   }
