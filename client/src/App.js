02ac02e11444f0756f7bb21fa14969f65f45abc1
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-06-26 13:44:49 -0700

    Set jwt token authentication in app

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import './App.css';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Navbar from './components/nav/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded))
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser())
+    store.dispatch(clearCurrentProfile())
+    window.location.href = '/login'
+  }
+}
 
 class App extends Component {
   render() {
